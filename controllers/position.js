f83554ec2a6e40fb6325b5bc20b1cecd1350b329
Apoluektov <Apoluektov@regard.ru>
2019-01-29 10:40:28 +0300

    lesson 5

---
+++ controllers/position.js
@@ -0,0 +1,12 @@
+module.exports.getByCategotyId = function (req,res) {
+
+}
+module.exports.create = function (req,res) {
+
+}
+module.exports.remove = function (req,res) {
+
+}
+module.exports.update = function (req,res) {
+
+}
