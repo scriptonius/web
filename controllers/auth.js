a04d9fcd01ab9f963c826011dc8c2de767a6f8a1
Apoluektov <Apoluektov@regard.ru>
2019-01-29 15:11:51 +0300

    lesson 6

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,9 @@ module.exports.login = function (req, res) {
 
 module.exports.register = function (req, res) {
     res.status(200).json({
-        register: 'from register'
+        login: {
+            email: req.body.email,
+            password: req.body.password
+        }
     })
 }
