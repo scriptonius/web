1d6072527bed142313ea1d2aa871b7059d20b0e4
Apoluektov <Apoluektov@regard.ru>
2019-02-08 10:13:50 +0300

    part 3 lesson 4

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,35 @@
-module.exports.login = function (req, res) {
-    res.status(200).json({
-        login: 'from controller'
-    })
+const User = requir
+
+const bycript = require('bcryptjs')
+
+//async перед функцией - значит, что будем обращаться к БД
+module.exports.login = async function (req, res) {
+
 }
 
-module.exports.register = function (req, res) {
-    res.status(200).json({
-        login: {
+module.exports.register = async function (req, res) {
+    // email password
+    const candidate =  await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        // Пользователь существует, нужно отправить ошибку
+        res.status(409).json({
+            message: 'Такой email уже занят. Попробуйте другой'
+        })
+    } else {
+         // нужно создать пользователя
+       const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
             email: req.body.email,
-            password: req.body.password
+            password: bcrypt.hashSync(password, salt)
+        })
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch(e) {
+            // Обработать ошибку
         }
-    })
+
+    }
 }
